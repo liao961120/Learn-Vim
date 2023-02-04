fbebfd61f139ab8eddcf5634375b5b2334b76a40
Yongfu <liao961120@gmail.com>
2023-02-04 21:59:09 +0800

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learn-Vim as quarto book
+
+[Learn-Vim](https://github.com/iggredible/Learn-Vim) compiled as quarto book.
+
+Read [here](https://yongfu.name/Learn-Vim/)
---
+++ build.sh
@@ -0,0 +1,9 @@
+[[ -d chapters ]] && rm -r chapters && mkdir chapters
+for file in Learn-Vim/ch*.md; do
+	fn=$(basename $file)
+	[[ $fn == "ch00_read_this_first.md" ]] && continue
+	cp $file chapters/"$fn".qmd
+done
+
+quarto render --html
+
